f38e22852e7d14c7c53cff71a698c2830c7b5b53
Kevin Camidge <kevincamidge@gmail.com>
2017-09-29 13:33:16 -0400

    Added token auth.

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,21 +1,35 @@
 import requests
+import base64
 
 
 class SpotifyAPI:
+    _client_id = "07281af1b6fe4be7b310550bf8cea3f9"
+    _client_secret = "5ec685a5ae6040a19582fc3dd357c903"
+
+    _token_endpoint = "https://accounts.spotify.com/api/token"
+    _api_endpoint = "https://api.spotify.com"
+    _search_endpoint = _api_endpoint + "/v1/search"
+    _artist_endpoint = _api_endpoint + "/v1/artists/"
 
     def __init__(self):
-        _client_id = "07281af1b6fe4be7b310550bf8cea3f9"
-        _client_secret = "5ec685a5ae6040a19582fc3dd357c903"
+        token = self.GetToken()
+        self._request_header = {'Accept': 'application/json',
+                                'Authorization': 'Bearer ' + token}
 
+    def GetToken(self):
+        client_string = self._client_id + ":" + self._client_secret
+        auth_header = {'Authorization': "Basic " + base64.b64encode(client_string.encode('utf-8')).decode(),
+                       'Content-Type': 'application/x-www-form-urlencoded'}
+        auth_body = [('grant_type', 'client_credentials')]
+        response = requests.post(self._token_endpoint, headers=auth_header, data=auth_body)
+        return response.json()['access_token']
 
-    def GetArtistIDFromSearch(self, p_artist, p_spotify_token):
+    def GetArtistIDFromSearch(self, p_artist):
         # Create the API request
         request_query = p_artist.replace(" ", "+")
-        request_url = 'https://api.spotify.com/v1/search?q=' + request_query + '&type=artist'
-        request_header = {'Accept': 'application/json',
-                          'Authorization': 'Bearer ' + p_spotify_token}
+        request_url = self._search_endpoint + '?q=' + request_query + '&type=artist'
         # Send request, get $response, and convert from json to dictionary
-        response = requests.get(request_url, headers=request_header)
+        response = requests.get(request_url, headers=self._request_header)
         json_artist_data = response.json()
         artist_data = json_artist_data['artists']['items']
         # Check if artist exists
@@ -29,21 +43,16 @@ class SpotifyAPI:
             # If the artist doesn't exist, return 0
             return 0
 
-    def GetAlbumsFromArtistID(self, p_artist_id, p_spotify_token):
+    def GetAlbumsFromArtistID(self, p_artist_id):
         # Create the API request
-        request_url = 'https://api.spotify.com/v1/artists/' + p_artist_id + '/albums'
-        request_header = {'Accept': 'application/json',
-                          'Authorization': 'Bearer ' + p_spotify_token}
+        request_url = self._artist_endpoint + p_artist_id + '/albums'
         # Send request, get $response, return as dictionary
-        response = requests.get(request_url, headers=request_header)
-        respone = response.json()
+        response = requests.get(request_url, headers=self._request_header)
         return response.json()['items']
 
-    def GetAlbumsFromSearch(self, p_album, p_spotify_token):
+    def GetAlbumsFromSearch(self, p_album):
         request_query = p_album.replace(" ", "+")
-        request_url = 'https://api.spotify.com/v1/search?q=' + request_query + '&type=album'
-        request_header = {'Accept': 'application/json',
-                          'Authorization': 'Bearer ' + p_spotify_token}
+        request_url = self._search_endpoint + '?q=' + request_query + '&type=album'
         # Send request, get $response, and convert from json to dictionary
-        response = requests.get(request_url, headers=request_header)
+        response = requests.get(request_url, headers=self._request_header)
         return response.json()['items']
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import os
 import json
 import SpotifyAPI
-import Deluxer
 
 
 crawler = SpotifyAPI.SpotifyAPI()
-deluxer = Deluxer.Deluxer()
-spotify_token = 'BQBWPyDIYgMIkBlGqdXm7LK9Lo15JB6AMbnZgTDvKR7dKjBdKF7Tg1hEh8clgpddhldrsR6uBPkkEX-B1nCCRrjVb4yJ0ghz-PHpke-BpkWchknxKX9toFEpQ7m7HOssWfxdgbfWvajQ_nlO'
 path_artists = 'test'
 
 # Read previously created dictionary into $data
@@ -16,12 +13,13 @@ data = json.loads(json_data)
 artist_ids = {}
 artists_not_found = []
 albums_not_found = {}
+tracks_not_found = {}
 deluxe_albums = {}
 
 # Iterate through directory, first level is artist folders
 for local_artist in os.listdir(path_artists):
     # Use artist name to find artist_id
-    artist_id = crawler.GetArtistIDFromSearch(local_artist, spotify_token)
+    artist_id = crawler.GetArtistIDFromSearch(local_artist)
     # If artist doesn't exist on Spotify, add to list and continue at next artist
     if artist_id == 0:
         artists_not_found.append(local_artist)
@@ -30,23 +28,22 @@ for local_artist in os.listdir(path_artists):
     # Artist exists and we have their $artist_id
 
     # Get all of artist's albums on Spotify
-    sp_albums_data = crawler.GetAlbumsFromArtistID(artist_id, spotify_token)
+    sp_albums_data = crawler.GetAlbumsFromArtistID(artist_id)
     # If the artist has no albums, continue at next artist
     if len(sp_albums_data) < 1:
         continue
     # Create list of all of artists album names
     sp_album_names = []
-    for sp_album in sp_albums_data['items']:
+    for sp_album in sp_albums_data:
         sp_album_names.append(sp_album['name'])
 
     # Iterate through directory, second level is album folders
     path_albums = path_artists + "\\" + local_artist
     for local_album in os.listdir(path_albums):
-        # Check if local album is in Spotify albums list
+        # If local album is in Spotify albums list...
         if any(local_album in s for s in sp_album_names):
-            # Local album is on Spotify...
             # Iterate through directory, third level is either song files or disc folders
-            path_songs = path_albums + "\\" + local_album
+            path_tracks = path_albums + "\\" + local_album
             #for local_song in os.listdir(path_songs):
             #    if os.path.isdir(local_song):
             #        # Disc Folder
