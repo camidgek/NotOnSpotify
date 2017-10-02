c0b9cdd25831f63628f98d16c14c16ada1ffcda5
Kevin Camidge <kevincamidge@gmail.com>
2017-10-02 17:56:04 -0400

    Moved client information to secret file.

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,28 +1,25 @@
 import requests
-import base64
+import secret
 
 
 class SpotifyAPI:
-    _client_id = "07281af1b6fe4be7b310550bf8cea3f9"
-    _client_secret = "5ec685a5ae6040a19582fc3dd357c903"
-
     _token_endpoint = "https://accounts.spotify.com/api/token"
     _api_endpoint = "https://api.spotify.com"
     _search_endpoint = _api_endpoint + "/v1/search"
     _artist_endpoint = _api_endpoint + "/v1/artists/"
 
     def __init__(self):
-        token = self.GetToken()
+        token = secret.getToken()
         self._request_header = {'Accept': 'application/json',
                                 'Authorization': 'Bearer ' + token}
 
-    def GetToken(self):
-        client_string = self._client_id + ":" + self._client_secret
-        auth_header = {'Authorization': "Basic " + base64.b64encode(client_string.encode('utf-8')).decode(),
-                       'Content-Type': 'application/x-www-form-urlencoded'}
-        auth_body = [('grant_type', 'client_credentials')]
-        response = requests.post(self._token_endpoint, headers=auth_header, data=auth_body)
-        return response.json()['access_token']
+    # def GetToken(self):
+    #     client_string = self._client_id + ":" + self._client_secret
+    #     auth_header = {'Authorization': "Basic " + base64.b64encode(client_string.encode('utf-8')).decode(),
+    #                    'Content-Type': 'application/x-www-form-urlencoded'}
+    #     auth_body = [('grant_type', 'client_credentials')]
+    #     response = requests.post(self._token_endpoint, headers=auth_header, data=auth_body)
+    #     return response.json()['access_token']
 
     def GetArtistIDFromSearch(self, p_artist):
         # Create the API request
--- main.py
+++ main.py
@@ -4,11 +4,7 @@ import SpotifyAPI
 
 
 crawler = SpotifyAPI.SpotifyAPI()
-path_artists = 'test'
-
-# Read previously created dictionary into $data
-json_data = open('data.txt').read()
-data = json.loads(json_data)
+path_artists = "G:\\iTunes\\Music"
 
 artist_ids = {}
 artists_not_found = []
@@ -19,6 +15,8 @@ deluxe_albums = {}
 # Iterate through directory, first level is artist folders
 for local_artist in os.listdir(path_artists):
     # Use artist name to find artist_id
+    if local_artist[-1] == "_":
+        local_artist = local_artist[:-1] + "."
     artist_id = crawler.GetArtistIDFromSearch(local_artist)
     # If artist doesn't exist on Spotify, add to list and continue at next artist
     if artist_id == 0:
@@ -27,39 +25,39 @@ for local_artist in os.listdir(path_artists):
 
     # Artist exists and we have their $artist_id
 
-    # Get all of artist's albums on Spotify
-    sp_albums_data = crawler.GetAlbumsFromArtistID(artist_id)
-    # If the artist has no albums, continue at next artist
-    if len(sp_albums_data) < 1:
-        continue
-    # Create list of all of artists album names
-    sp_album_names = []
-    for sp_album in sp_albums_data:
-        sp_album_names.append(sp_album['name'])
-
-    # Iterate through directory, second level is album folders
-    path_albums = path_artists + "\\" + local_artist
-    for local_album in os.listdir(path_albums):
-        # If local album is in Spotify albums list...
-        if any(local_album in s for s in sp_album_names):
-            # Iterate through directory, third level is either song files or disc folders
-            path_tracks = path_albums + "\\" + local_album
-            #for local_song in os.listdir(path_songs):
-            #    if os.path.isdir(local_song):
-            #        # Disc Folder
-            #    else:
-            #        # Check if song is liked on Spotify
-
-            #https: // api.spotify.com / v1 / me / tracks / contains
-
-        else:
-            albums_not_found[local_artist] = local_album
-    # Now have a list of all local albums not found on Spotify
-
+#     # Get all of artist's albums on Spotify
+#     sp_albums_data = crawler.GetAlbumsFromArtistID(artist_id)
+#     # If the artist has no albums, continue at next artist
+#     if len(sp_albums_data) < 1:
+#         continue
+#     # Create list of all of artists album names
+#     sp_album_names = []
+#     for sp_album in sp_albums_data:
+#         sp_album_names.append(sp_album['name'])
+#
+#     # Iterate through directory, second level is album folders
+#     path_albums = path_artists + "\\" + local_artist
+#     for local_album in os.listdir(path_albums):
+#         # If local album is in Spotify albums list...
+#         if any(local_album in s for s in sp_album_names):
+#             # Iterate through directory, third level is either song files or disc folders
+#             path_tracks = path_albums + "\\" + local_album
+#             #for local_song in os.listdir(path_songs):
+#             #    if os.path.isdir(local_song):
+#             #        # Disc Folder
+#             #    else:
+#             #        # Check if song is liked on Spotify
+#
+#             #https: // api.spotify.com / v1 / me / tracks / contains
+#
+#         else:
+#             albums_not_found[local_artist] = local_album
+#     # Now have a list of all local albums not found on Spotify
+#
 with open('artists.txt', 'w') as outfile:
     json.dump(artists_not_found, outfile, indent=4)
-
-with open('albums.txt', 'w') as outfile:
-    json.dump(albums_not_found, outfile, indent=4)
-
-print(deluxe_albums)
+#
+# with open('albums.txt', 'w') as outfile:
+#     json.dump(albums_not_found, outfile, indent=4)
+#
+# print(deluxe_albums)
